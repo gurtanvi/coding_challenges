fb6bbebfd2eee8e3c8c078cbf86ba03e81000097
gurtanvi <gurtanvi@gmail.com>
2021-09-09 13:38:30 +0200

    added capitalize, list and phonebook solutions

---
+++ capitalize.py
@@ -0,0 +1,11 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+input_name = input().split(' ')
+print(' '.join((word.capitalize() for word in input_name)))
+
+
